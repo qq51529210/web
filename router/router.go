0a3957c0bbb07f539e56a3968c85b62e341dfd41
LinWenBin <51529210@qq.com>
2022-01-04 19:54:47 +0800

    u

--- router/router.go
+++ router/router.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"sync"
 )
 
 const (
@@ -157,4 +158,95 @@ func (r *router) Static(routePath, file string, cache bool) {
 	for i := 0; i < len(fis); i++ {
 		r.Static(path.Join(routePath, fis[i].Name()), filepath.Join(file, fis[i].Name()), cache)
 	}
+}
+
+type subRouter struct {
+	path string
+	Router
+}
+
+type RootRouter interface {
+	http.Handler
+	Router
+	SubRouter(routePath string) Router
+	Global(handle ...HandleFunc)
+	NotFound(handle ...HandleFunc)
+	// Add static file handlers.
+	// If file is a directory, add all sub files, routePath is root route of these handlers.
+	// If file name is "index.html", add extra route "/".
+	// If cache is true, use CachaHandler, else use FileHandler.
+	Static(routePath, file string, cache bool)
+}
+
+func NewRootRouter() RootRouter {
+	r := new(rootRouter)
+	r.ctx.New = func() interface{} {
+		return new(Context)
+	}
+	return r
+}
+
+type rootRouter struct {
+	router
+	global   []HandleFunc
+	notfound []HandleFunc
+	ctx      sync.Pool
+}
+
+func (r *rootRouter) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	ctx := r.ctx.Get().(*Context)
+	ctx.Request = req
+	ctx.ResponseWriter = res
+	ctx.Data = nil
+	ctx.globalFunc = r.global
+	ctx.globalIdx = 0
+	ctx.handleIdx = 0
+	//
+	var route *route
+	if req.Method[0] == 'G' {
+		route = r.root[_METHOD_GET].Match(ctx)
+	} else if req.Method[0] == 'H' {
+		route = r.root[_METHOD_HEAD].Match(ctx)
+	} else if req.Method[0] == 'D' {
+		route = r.root[_METHOD_DELETE].Match(ctx)
+	} else if req.Method[0] == 'C' {
+		route = r.root[_METHOD_CONNECT].Match(ctx)
+	} else if req.Method[0] == 'O' {
+		route = r.root[_METHOD_OPTIONS].Match(ctx)
+	} else if req.Method[0] == 'T' {
+		route = r.root[_METHOD_TRACE].Match(ctx)
+	} else if req.Method[1] == 'O' {
+		route = r.root[_METHOD_POST].Match(ctx)
+	} else if req.Method[1] == 'U' {
+		route = r.root[_METHOD_PUT].Match(ctx)
+	} else if req.Method[1] == 'A' {
+		route = r.root[_METHOD_PATCH].Match(ctx)
+	}
+	if route == nil {
+		ctx.handleFunc = r.notfound
+	} else {
+		ctx.handleFunc = route.handleFunc
+	}
+	ctx.Next()
+	r.ctx.Put(ctx)
+}
+
+func (r *rootRouter) NotFound(handle ...HandleFunc) {
+	if len(handle) != 0 {
+		r.notfound = handle
+	} else {
+		r.notfound = []HandleFunc{
+			func(ctx *Context) {
+				ctx.ResponseWriter.WriteHeader(http.StatusNotFound)
+			},
+		}
+	}
+}
+
+func (r *rootRouter) Global(handle ...HandleFunc) {
+	r.global = handle
+}
+
+func (r *rootRouter) SubRouter(routePath string) Router {
+	return &subRouter{path: routePath, Router: r}
 }
