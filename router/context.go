e20894880ade58c1cc561dcc6289212e46bad553
LinWenBin <51529210@qq.com>
2022-01-05 02:25:26 +0800

    u

--- router/context.go
+++ router/context.go
@@ -2,7 +2,23 @@ package router
 
 import (
 	"bytes"
+	"encoding/json"
+	"io"
+	"mime"
 	"net/http"
+	"strings"
+)
+
+const (
+	bearerTokenPrefix = "Bearer "
+)
+
+var (
+	ContentTypeUTF8 = "charset=utf-8"
+	ContentTypeJSON = mime.TypeByExtension(".json")
+	ContentTypeHTML = mime.TypeByExtension(".html")
+	ContentTypeJS   = mime.TypeByExtension(".js")
+	ContentTypeCSS  = mime.TypeByExtension(".css")
 )
 
 type Context struct {
@@ -21,4 +37,29 @@ func (ctx *Context) Next() {
 		ctx.handleIdx++
 		f(ctx)
 	}
+}
+
+// Return header["Authorization"] Bearer token.
+func (c *Context) BearerToken() string {
+	token := c.Request.Header.Get("Authorization")
+	if token == "" || !strings.HasPrefix(token, bearerTokenPrefix) {
+		return ""
+	}
+	return token[len(bearerTokenPrefix):]
+}
+
+// Set Content-Type and statusCode, convert data to JSON and write to body.
+func (c *Context) WriteJSON(statusCode int, value interface{}) error {
+	c.ResponseWriter.WriteHeader(statusCode)
+	c.ResponseWriter.Header().Set("Content-Type", ContentTypeJSON)
+	enc := json.NewEncoder(c.ResponseWriter)
+	return enc.Encode(value)
+}
+
+// Set Content-Type and statusCode, write text to body.
+func (c *Context) WriteHTML(statusCode int, text string) error {
+	c.ResponseWriter.WriteHeader(statusCode)
+	c.ResponseWriter.Header().Set("Content-Type", ContentTypeHTML)
+	_, err := io.WriteString(c.ResponseWriter, text)
+	return err
 }
